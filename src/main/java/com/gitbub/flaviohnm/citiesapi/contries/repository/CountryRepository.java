6cfd8c2c3526441a90ed3ac427df55942d6497a6
Flavio Monteiro <flaviohnm@gmail.com>
2021-06-02 16:59:06 -0300

    projeto cities em java

---
+++ src/main/java/com/gitbub/flaviohnm/citiesapi/contries/repository/CountryRepository.java
@@ -0,0 +1,9 @@
+package com.gitbub.flaviohnm.citiesapi.contries.repository;
+
+
+import com.gitbub.flaviohnm.citiesapi.contries.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CountryRepository extends JpaRepository<Country, Long> {
+
+}
